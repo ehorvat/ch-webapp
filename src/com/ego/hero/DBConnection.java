9312faa66289d10ff2ab8fc2a0606c199c9a056e
ehorvat <eric.horvat@gmail.com>
2015-02-27 18:40:33 -0800

    First Commit

---
+++ src/com/ego/hero/DBConnection.java
@@ -0,0 +1,27 @@
+package com.ego.hero;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+
+import com.ego.util.DBConnect;
+
+public class DBConnection {
+	
+	static DBConnect connection;
+	
+	public static Connection dbc() throws NamingException, SQLException,ServletException {
+		try {
+			connection = new DBConnect("classroomhero", null);
+
+			return connection.connect();
+		}
+
+		catch (Exception e) {
+			throw new ServletException(e);
+		}
+	}
+
+}
